ac5e20a4bc9c5c0aa2ca6d97e924503bf3e5ae36
Dennis Priskorn <egils@riseup.net>
2024-05-18 08:21:07 +0200

    feat: Add Procfile, update script and generate pinned requirements for the cloud native builder. Also update the dependencies.

--- api.py
+++ api.py
@@ -1,8 +1,7 @@
 import json
-
-from fastapi import FastAPI, HTTPException
 import os
 
+from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
 app = FastAPI()
@@ -17,10 +16,11 @@ def redirect_to_docs():
 
 @app.get("/v2/{id_}")
 def read_data(id_: str):
+    """It returns 200 and empty dictionary if the record is not found"""
     data_file = os.path.join("data", "v2", f"{id_}.json")
     if os.path.exists(data_file):
         with open(data_file, 'r') as f:
             data = json.load(f)
         return data
     else:
-        raise HTTPException(status_code=200, detail="Data not found")
+        return {}
